c01e061f8f0af200c743b9ba34e81fbfa1de9468
Spohn <jonathan.spohn@sparcedge.com>
2012-11-28 18:26:02 -0500

    deleting works

--- SupplyFinder/EditDeleteViewController.m
+++ SupplyFinder/EditDeleteViewController.m
@@ -27,6 +27,9 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view.
+    //get managed object context from the app delegate
+    _appDelegate = [[UIApplication sharedApplication] delegate];
+    _managedObjectContext = [_appDelegate managedObjectContext];
     
     [_titleField setText:[_pin title]];
     
@@ -58,7 +61,61 @@
 }
 
 - (IBAction)backPressed:(id)sender {
+    
+    [_pin setTitle:[_titleField text]];
+    
+    [_managedObjectContext save:nil];
+    
+    NSArray *ants = [[(ViewController *)_delegate mapView] annotations];
+    
+    [[(ViewController *)_delegate mapView] removeAnnotations:ants];
+    
+    [[(ViewController *)_delegate mapView] addAnnotations:ants];
+    
     [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+- (IBAction)segmentChanged:(id)sender {
+    
+    NSInteger selectedIndex = [_categorySegment selectedSegmentIndex];
+    
+    [_pin setCategory:[NSNumber numberWithInteger:selectedIndex]];
+    
+    if(selectedIndex == 0){
+        //food
+        [_catImage setImage:[UIImage imageNamed:@"food.png"]];
+    }else if(selectedIndex == 1){
+        //weapons
+        [_catImage setImage:[UIImage imageNamed:@"gun icon.png"]];
+    }else if(selectedIndex == 2){
+        //fuel
+        [_catImage setImage:[UIImage imageNamed:@"oil icon.png"]];
+    }else if(selectedIndex == 3){
+        //zombies
+        [_catImage setImage:[UIImage imageNamed:@"zombie.png"]];
+    }else if(selectedIndex == 4){
+        //zombies
+        [_catImage setImage:[UIImage imageNamed:@"companionship.png"]];
+    }
+    
+}
+
+- (IBAction)deletePressed:(id)sender {
+    
+    [_managedObjectContext deleteObject:_pin];
+    
+    [_managedObjectContext save:nil];
+    
+    NSArray *ants = [[(ViewController *)_delegate mapView] annotations];
+    
+    [[(ViewController *)_delegate mapView] removeAnnotations:ants];
+    
+    [[(ViewController *)_delegate mapView] addAnnotations:ants];
+    
+    [self dismissViewControllerAnimated:YES completion:nil];
+    
+    
+    
 }
 
 @end
