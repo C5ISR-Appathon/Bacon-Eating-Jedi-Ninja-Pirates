1a41fcf6c097cb4a6c285c90d18ea0e56e74fe16
Spohn <jonathan.spohn@sparcedge.com>
2012-11-28 17:31:09 -0500

    syncing

---
+++ SupplyFinder/EditDeleteViewController.h
@@ -0,0 +1,17 @@
+//
+//  EditDeleteViewController.h
+//  SupplyFinder
+//
+//  Created by Jonathan Spohn on 11/28/12.
+//  Copyright (c) 2012 SPARC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "Pin.h"
+#import "AppDelegate.h"
+
+@interface EditDeleteViewController : UIViewController
+
+
+
+@end
---
+++ SupplyFinder/EditDeleteViewController.m
@@ -0,0 +1,38 @@
+//
+//  EditDeleteViewController.m
+//  SupplyFinder
+//
+//  Created by Jonathan Spohn on 11/28/12.
+//  Copyright (c) 2012 SPARC. All rights reserved.
+//
+
+#import "EditDeleteViewController.h"
+
+@interface EditDeleteViewController ()
+
+@end
+
+@implementation EditDeleteViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
