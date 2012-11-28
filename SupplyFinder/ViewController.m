686c79ea633e0d40fa95c297649493dc2d43f419
Bobby Schuchert <bobby.schuchert@sparcedge.com>
2012-11-28 13:06:54 -0500

    Map should now zoom to user's location-izzle

--- SupplyFinder/ViewController.m
+++ SupplyFinder/ViewController.m
@@ -68,6 +68,10 @@
     for (Pin *savedPin in self.pins) {
         [self.mapView addAnnotation:savedPin];
     }
+    
+    self.mapView.showsUserLocation = YES;
+    
+    
 }
 
 - (void)didReceiveMemoryWarning
@@ -359,7 +363,18 @@
 }
 
 
-
+- (void)mapView:(MKMapView *)aMapView didUpdateUserLocation:(MKUserLocation *)aUserLocation {
+    MKCoordinateRegion region;
+    MKCoordinateSpan span;
+    span.latitudeDelta = 0.095;
+    span.longitudeDelta = 0.095;
+    CLLocationCoordinate2D location;
+    location.latitude = aUserLocation.coordinate.latitude;
+    location.longitude = aUserLocation.coordinate.longitude;
+    region.span = span;
+    region.center = location;
+    [aMapView setRegion:region animated:YES];
+}
 
 
 
