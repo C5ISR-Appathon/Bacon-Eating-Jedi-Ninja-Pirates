92988767679b42998ed09fb5f0a53e7c3229494e
Spohn <jonathan.spohn@sparcedge.com>
2012-11-28 12:40:22 -0500

    starting new controller

---
+++ SupplyFinder/PinCreateEditViewController.h
@@ -0,0 +1,15 @@
+//
+//  PinCreateEditViewController.h
+//  SupplyFinder
+//
+//  Created by Jonathan Spohn on 11/28/12.
+//  Copyright (c) 2012 SPARC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PinCreateEditViewController : UIViewController
+
+@property (strong, nonatomic) IBOutlet UISegmentedControl *categorySegment;
+
+@end
